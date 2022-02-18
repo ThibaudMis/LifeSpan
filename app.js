49eaa14fbf7f6222a257e974b0198d3d35969599
thibaud <misthibaud@gmail.com>
2022-02-18 15:34:31 +0100

    debug language issue

--- app.js
+++ app.js
@@ -75,6 +75,15 @@ function textprint(){
     document.getElementById("013").innerText=currentlanguage[12];
     document.getElementById("014").innerText=currentlanguage[13];
     if(lifespan!=null){showreponce()}
-    showData()
-    }   
+    
+    Array.from(document.getElementsByClassName('txtOutclass01')).forEach(item => item.innerText=currentlanguage[14])
+    Array.from(document.getElementsByClassName('txtOutclass02')).forEach(item => item.innerText=currentlanguage[15])
+    Array.from(document.getElementsByClassName('txtOutclass03')).forEach(item => item.innerText=currentlanguage[16])
+    if( thislotnumber[9]){
+    Array.from(document.getElementsByClassName('txtInclass01')).forEach(item => item.innerText=currentlanguage[17])
+    Array.from(document.getElementsByClassName('txtInclass02')).forEach(item => item.innerText=currentlanguage[18])
+    Array.from(document.getElementsByClassName('txtInclass03')).forEach(item => item.innerText=currentlanguage[19])
+} 
+}
+
 
